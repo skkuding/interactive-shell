a471ab95090b411d3da22218f76c648378623b8d
anzanda <tjd1024@g.skku.edu>
2022-01-13 23:36:25 +0900

    Add memory, time limt arguments

--- server/formatter.js
+++ server/formatter.js
@@ -22,14 +22,18 @@ function makeRunFormat (dir, lang) {
     const run_config = lang_config[lang].run;
     const seccomp_rule = "--seccomp_rule_name=" + run_config.seccomp_rule;
     const env = "--env=" + run_config.env;
+    const max_cpu_time = "--max_cpu_time=" + String(60*1000)
+    const max_real_time = "--max_real_time=" + String(60*1000)
+    const max_memory = "--max_memory=" + String(256*1024*1024)
     var exe_path = WORKSPACE_BASE + _dir + "/" + exe_name;
     if (lang === "py2") {
-        return ["--exe_path=/usr/bin/python2", "--args="+exe_path, seccomp_rule];
+        return ["--exe_path=/usr/bin/python2", "--args="+exe_path, max_cpu_time, max_real_time, max_memory, seccomp_rule];
     }
     if (lang === "py3") {
-        return ["--exe_path=/usr/bin/python3", "--args="+exe_path, seccomp_rule];
+        return ["--exe_path=/usr/bin/python3", "--args="+exe_path, max_cpu_time, max_real_time, max_memory, seccomp_rule];
     }
     exe_path = "--exe_path=" + exe_path;
-    return [exe_path, seccomp_rule, env];
+
+    return [exe_path, max_cpu_time, max_real_time, max_memory, seccomp_rule, env];
 }
 module.exports = {purifyPath, checkLanguage, makeRunFormat}
