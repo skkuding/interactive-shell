0750d55e9257b6db9b7674597c135e8cf793933a
minseo999 <alstj778@gmail.com>
2022-01-26 03:58:55 +0900

    Add limiter to socket connection request -- identify user by a session id -- set cors setting both client and server

--- server/server.js
+++ server/server.js
@@ -5,16 +5,19 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
     cors: {
-        origin: "*",
+        origin: "http://localhost:8080",
         credentials: true
     }
 });
 const pty = require("node-pty");
 const jsonParser = require('body-parser').json();
+const expSession = require("express-session");
+const ioSession = require("express-socket.io-session");
+const rateLimit = require("rate-limiter-flexible");
 
 const logging = require('./winston');
 const morgan = require('morgan');
-const combined = ':remote-addr - :remote-user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"' 
+const combined = ':remote-addr - :remote-user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'
 const morganFormat = process.env.NODE_ENV !== "production" ? "dev" : combined;
 // morgan 출력 형태 server.env에서 NODE_ENV 설정 production : 배포 dev : 개발
 
@@ -27,9 +30,23 @@ let server_logger = new logging("server")
 let compile_logger = new logging("compile")
 let run_logger = new logging("run")
 
+const limiter = new rateLimit.RateLimiterMemory({
+    points: 20, // Limit each sessionID to 20 requests
+    duration: 60, // For 1 minute
+});
+const session = expSession({
+    secret: "secretKey",
+    resave: false,
+    saveUninitialized: true
+});
+
 // TODO: redirect errors to log file
-app.use(require('cors')());
+app.use(require('cors')({
+    origin: "http://localhost:8080",
+    credentials: true
+}));
 app.use(jsonParser);
+app.use(session);
 
 app.use( morgan(morganFormat, {stream : server_logger.stream}) );
 
@@ -53,15 +70,33 @@ app.post("/compile", async (req, res) => {
 })
 
 //TODO: handshake, Run 분리
+io.use(ioSession(session, {autoSave: true}))
 io.on("connection", async(socket) => {
+    var dir = socket.handshake.query['token'];
+    var lang = socket.handshake.query['lang'];
+    var sid = socket.handshake.sessionID;
+
+    //Check Rate Limit socket connection request
     try {
-        var dir = socket.handshake.query['token'];
-        var lang = socket.handshake.query['lang'];
+        await limiter.consume(sid);
+    } catch (err) {
+        socket.emit('stdout', "too many request");
+        try {
+            await cleanUp(dir);
+        } catch (err) {
+            console.log(err);
+        } finally {
+            socket.emit("exited");
+            socket.disconnect();
+        }
+    }
+
+    try {
         await purifyPath(dir).then((value) => { dir = value; })
         await checkLanguage(lang).then((value) => { if(!value) throw new Error("Unsupported language"); })
 
         if(!fs.existsSync(BASE_DIR + dir)) {
-            socket.disconnect(); 
+            socket.disconnect();
         } else {
             const shell = pty.spawn("/usr/lib/judger/libjudger.so", makeRunFormat(dir, lang));
             shell.on('data', (data) => {
