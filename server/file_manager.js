2c9f915422dd88a996ae38ebc8693117c79436fa
HAKSAN <cranemont@gmail.com>
2022-01-29 16:32:47 +0900

    refactor: remove duplicated directory existence check

--- server/file_manager.js
+++ server/file_manager.js
@@ -4,11 +4,8 @@ const BASE_DIR = require("./constants").WORKSPACE_BASE;
 async function cleanUp(dir) {
     var _dir = BASE_DIR + dir
     try {
-        if(fs.existsSync(_dir)){
-            await fs.promises.rm(_dir, { recursive: true, force: true });
-        }
+        await fs.promises.rm(_dir, { recursive: true, force: true });
     } catch (err) {
-        console.log(err);
         throw err;
     }
 }
